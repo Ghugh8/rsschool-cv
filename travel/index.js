5c2ea8459f87b598e910ac35a56e3e70724363ed
ghugh8 <ghugh87@gmail.com>
2022-07-23 23:12:28 +0300

    feat: add pop-up login

--- travel/index.js
+++ travel/index.js
@@ -12,4 +12,34 @@ window.onload=function() {
   document.querySelector('.navigation_list').addEventListener('click', () => {
     document.querySelector('.navigation').classList.remove('navigation_active');
 })
+
+const loginButton = document.querySelector('.login');
+const accountButton = document.querySelector('.account');
+const popup = document.querySelector('.pop-up-desk');
+const popupMob = document.querySelector('.pop-up');
+const bodyHidden = document.querySelector('.body');
+
+loginButton.addEventListener('click', () => {
+  popup.classList.toggle('hidden')
+  bodyHidden.classList.toggle('overflow')
+})
+
+popup.addEventListener('click', (event) => {
+  if (event.target.classList.contains('pop-up-desk')) {
+      popup.classList.toggle('hidden')
+      bodyHidden.classList.toggle('overflow')
+  }
+})
+
+accountButton.addEventListener('click', () => {
+  popupMob.classList.toggle('hidden')
+  bodyHidden.classList.toggle('overflow')
+})
+
+popupMob.addEventListener('click', (event) => {
+  if (event.target.classList.contains('pop-up')) {
+      popupMob.classList.toggle('hidden')
+      bodyHidden.classList.toggle('overflow')
+  }
+})
 }
